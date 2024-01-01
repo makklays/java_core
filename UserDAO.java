7e536f704fd07f5c28b1b22348ad9d000f5354ec
Alexander Kuziv <makklays@gmail.com>
2024-01-01 23:44:41 +0100

    Update UserDAO.java

--- UserDAO.java
+++ UserDAO.java
@@ -4,7 +4,36 @@
 
 class UserDAO 
 {
-  void UserDAO() {
-    // todo
+  void UserDAO() {}
+
+  private boolean InsertUser(User user) {
+    // insert
   }
+
+  private boolean UpdateUser(User user) {
+    // update 
+  } 
+
+  private boolean DeleteUser(User user) {
+    // delete
+  }
+
+  public String getUserInfo() {
+        return "User {" +
+            "id=" + id +
+            ", login='" + login + '\'' +
+            ", firstname='" + firstname + '\'' +
+            ", lastname='" + lastname + '\'' +
+            ", password='" + password + '\'' +
+            ", gender='" + gender + '\'' +
+            ", phone='" + phone + '\'' +
+            ", email='" + email + '\'' +
+            ", city='" + city + '\'' +
+            ", auth_at='" + isAuth + '\'' +
+            ", code='" + code + '\'' +
+            ", created_at='" + createdAt + '\'' +
+            ", updated_at=" + updatedAt +
+        '}';
+    }
 }
+
