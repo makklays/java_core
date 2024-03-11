8206ebd2ec0e02feb4af42b8d0bf758850b2a30e
Alexander Kuziv <makklays@gmail.com>
2024-03-11 21:59:18 +0100

    Create Scanner.java

---
+++ Scanner.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Solution {
+   public static void main(String[] args)
+   {
+      String str = "100 200 400 600";
+      Scanner scanner = new Scanner(str);
+      int a = scanner.nextInt();
+      int b = scanner.nextInt();
+
+      System.out.println(a + b);
+   }
+}
+
