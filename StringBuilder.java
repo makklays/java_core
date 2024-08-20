f111a87be5298ce0af7f1bcfecef37be2e94a4e2
Alexander Kuziv <makklays@gmail.com>
2024-08-21 00:10:24 +0200

    Create StringBuilder.java

---
+++ StringBuilder.java
@@ -0,0 +1,25 @@
+
+//-- ¿Donde esta mi torro bull ? --
+
+StringBuilder bull = new StringBuilder("Me llamo Alexander Kuziv.. Pues, soy Alexander Kuziv.. Mi nombre Alexander Kuziv..");
+if (bull.indexOf("Alexander", 0)) { // true
+    System.out.println("Match - coincide!");
+} 
+if (bull.indexOf("Alex", 0)) { // true
+    System.out.println("match - coincide!");
+} 
+if (!bull.indexOf("Alena", 0)) { // !false
+    System.out.println("not match - no coincide!");
+}
+if (bull.indexOf("Ale", 0)) { // true  
+    System.out.println("match - coincide!");
+}
+
+StringBuilder bullGold = new StringBuilder("Alena");
+bullGold.reverse(); // П anelA ))
+
+//
+// Пирс Бросмен смотрит на Штиль Швайгера - крассавчик ! )))
+//
+
+//-- ¿ Yo busco nuevo punto o viejo ? -- 
