2d0c4b8e0fe2ea77ebca573982c5fccdfcf21fe3
Alexander Kuziv <makklays@gmail.com>
2024-08-21 18:52:33 +0200

    Update ScannerConsole.java

--- ScannerConsole.java
+++ ScannerConsole.java
@@ -4,6 +4,7 @@ Scanner console = new Scanner(System.in);
 String line = console.nextLine(); // читаем с клавиатуры строку 
 int number = console.nextInt(); // читаем с клавиатуры число 
 double number = console.nextDouble();
+String palabra = console.next(); // читает одно слово 
 
 System.out.println("Ввели строку: " + line);
 System.out.println("Ввели число: " + number);
