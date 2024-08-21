eb75862799b9bc0ca814c5ac7946bbcf91c6a171
Alexander Kuziv <makklays@gmail.com>
2024-08-21 18:50:00 +0200

    Create ScannerConsole.java

---
+++ ScannerConsole.java
@@ -0,0 +1,10 @@
+
+// Scanner from keyboard 
+Scanner console = new Scanner(System.in);
+String line = console.nextLine(); // читаем с клавиатуры строку 
+Integer number = console.nextInt(); // читаем с клавиатуры число 
+
+System.out.println("Ввели строку: " + line);
+System.out.println("Ввели число: " + number);
+
+.
