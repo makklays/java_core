922379ca1f63f0553aa679284b5b672a0dafc5cb
Alexander Kuziv <makklays@gmail.com>
2024-08-21 18:43:22 +0200

    Create CompareLinkAndValue.java

---
+++ CompareLinkAndValue.java
@@ -0,0 +1,14 @@
+
+// compare link 
+
+String a = "aaa";
+String b = "bbb";
+System.out.println(a == b); // false
+
+// compare value 
+
+String a = "aaa";
+String b = "aaa";
+System.out.println(a == b); // false 
+System.out.println(a.equels(b)); // true :-)
+
