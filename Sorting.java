3d2a6441525d382bf9b3b2f9a6f4db79924ba811
Alexander Kuziv <makklays@gmail.com>
2024-10-06 23:52:27 +0200

    Update Sorting.java

--- Sorting.java
+++ Sorting.java
@@ -52,3 +52,44 @@ sortedMap.putAll(unsortedMap);
 в коллекции время поиска не удваивается, а увеличивается на постоянную величину (прим. перев.)
 
 //
+
+//добавляем возможность сравнивать об'єкты User
+public class User  implements Comparable <User> {
+
+    private String name;
+    private Integer age;
+    private String email;
+
+    public User(String name, int age, String email) {
+        this.name = name;
+        this.age = age;
+        this.email = email;
+    }
+
+    @Override
+    //реализуем метод compareTo интерфейса Comparable
+    public int compareTo(User o) {
+
+    	//используем метод compareTo из класса String для сравнения имен
+        int result = this.name.compareTo(o.name);
+
+	//если имена одинаковые -  сравниваем возраст,
+	//используя метод compareTo из класса Integer
+
+        if (result == 0) {
+            result = this.age.compareTo(o.age);
+        }
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", email='" + email + '\'' +
+                '}';
+     }
+}
+
+// 
