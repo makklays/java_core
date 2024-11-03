f1452762bc5dba2b6060243079036ec4b0a6c8a4
Alexander Kuziv <makklays@gmail.com>
2024-11-03 15:35:27 +0100

    Update LocalDate.java

--- LocalDate.java
+++ LocalDate.java
@@ -1,9 +1,12 @@
 
-// The java.time package
+// The java.time package 
 // The LocalDateTime class 
 
 // current date 
-LocalDate myObj = LocalDate.now();
+LocalDate myObj = LocalDate.now(); 
+
+// 2024-11-03 
+System.out.println(myObj); 
 
 
 //
