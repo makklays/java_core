23af07d9f92ca48fe6d7c5c24316ab7ad0b38e8d
Alexander Kuziv <makklays@gmail.com>
2026-02-17 00:22:02 +0100

    Create BinarySearch.java

---
+++ algorithms/BinarySearch.java
@@ -0,0 +1,43 @@
+
+/**
+ * Binary Search 
+ * 
+ * Complexity time:
+ * best case    - O(1)
+ * average case - O(log n)
+ * worst case   - O(log n)
+ */
+public class BinarySearch {
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 4, 7, 9, 11, 12};
+        int target = 7;
+
+        int index = binarySearch(arr, target);
+
+        if (index != -1) {
+            System.out.println("Element found at index: " + index);
+        } else {
+            System.out.println("Element not found");
+        }
+    }
+
+    public static int binarySearch(int[] arr, int target) {
+        int low = 0;
+        int high = arr.length - 1;
+
+        while (low < high) {
+            int mid = low + (high + low) / 2;
+
+            if (arr[mid] == target) {
+                return mid;           // element found 
+            } else if (arr[mid] < target) {
+                low = arr[mid] + 1;   // search in the right half
+            } else {
+                high = arr[mid] - 1;  // search in the left half
+            }
+        }
+
+        return -1;  // element not found
+    }
+}
+
