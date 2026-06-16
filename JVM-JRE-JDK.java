8222098f39f7a3e45cb8ea4e59a33445bfb11dd6
Alexander Kuziv <makklays@gmail.com>
2026-06-16 10:23:34 +0200

    Update JVM-JRE-JDK.java

--- JVM-JRE-JDK.java
+++ JVM-JRE-JDK.java
@@ -1,4 +1,12 @@
+/**
+ * company: TechMatrix18
+ * author: Alexander Kuziv 
+ * date: 01.05.2026
+ */
 
 Java Virtual Machine (JVM) входит в -> Java Runtime Enviroment (JRE) входит в -> Java Develipment Kit (JDK) 
 
+  
 .
+
+  
